02c55f0afaebf72be83b5fd125c3095659e2f0f2
karol701 <karol.liszczyk@galacticpicture.com>
2015-06-21 00:41:00 +0200

    Finish log in/log out

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -52,4 +52,8 @@ class UserTests < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
+
 end
